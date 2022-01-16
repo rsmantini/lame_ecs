69118c06e3f27945ea055fa533f872f0cc5c6ae4
Rodrigo Mantini <rodrigo.mantini@gmail.com>
2022-01-16 02:05:20 +0100

    introducing macros

---
+++ lame-ecs-macro/src/lib.rs
@@ -0,0 +1,201 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{
+    parse::Parser, parse_macro_input, spanned::Spanned, Data, DeriveInput, Error, Fields, Ident,
+};
+
+#[proc_macro]
+pub fn create_component_collection_struct(input: TokenStream) -> TokenStream {
+    let parser =
+        syn::punctuated::Punctuated::<syn::Ident, syn::Token![,]>::parse_separated_nonempty;
+    let types = match parser.parse(input) {
+        Ok(t) => t,
+        Err(e) => {
+            return TokenStream::from(e.to_compile_error());
+        }
+    };
+    let types: Vec<syn::Ident> = types.into_iter().collect();
+    let field_names: Vec<syn::Ident> = types
+        .iter()
+        .map(|x| syn::Ident::new(&(x.to_string().to_lowercase() + "s"), x.span()))
+        .collect();
+    let expanded = quote! {
+        #[derive(Default, LameEcsComponents)]
+        struct LameEcsComponentCollection {
+            #(#field_names: Vec<Option<#types>>,)*
+        }
+
+
+    };
+    TokenStream::from(expanded)
+}
+
+#[proc_macro_derive(LameEcsComponents)]
+pub fn derive_lame_ecs_components_fn(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let struct_data = match &input.data {
+        Data::Struct(s) => s,
+        _ => {
+            return TokenStream::from(
+                Error::new(input.span(), "expected struct").to_compile_error(),
+            );
+        }
+    };
+    let field_names = match get_field_names(struct_data) {
+        Some(f) => f,
+        None => {
+            return TokenStream::from(
+                Error::new(input.span(), "expected named fields").to_compile_error(),
+            );
+        }
+    };
+    let types = match get_component_types(struct_data) {
+        Some(f) => f,
+        None => {
+            return TokenStream::from(
+                Error::new(input.span(), "expected Vec<Optional<Type>>").to_compile_error(),
+            );
+        }
+    };
+    let ident = &input.ident;
+    let expanded = quote! {
+        impl Components for #ident {
+            fn push_none(&mut self) {
+                #(self.#field_names.push(None);)*
+            }
+            fn remove(&mut self, index: usize) {
+                #(self.#field_names.remove(index);)*
+            }
+
+            fn as_any(&self) -> &dyn std::any::Any {
+                self
+            }
+
+            fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+                self
+            }
+        }
+
+        #(impl Component for #types {
+            fn get_vec(components: &mut dyn Components) -> &mut Vec<Option<Self>> {
+                &mut downcast_components_mut::<LameEcsComponentCollection>(components).#field_names
+            }
+        })*
+
+    };
+    TokenStream::from(expanded)
+}
+
+#[proc_macro]
+pub fn get_component_collection(input: TokenStream) -> TokenStream {
+    let world = parse_macro_input!(input as Ident);
+    let expanded = quote! {
+        downcast_components_mut::<LameEcsComponentCollection>(#world.components.as_mut())
+    };
+    TokenStream::from(expanded)
+}
+
+#[proc_macro]
+pub fn component_iter(input: TokenStream) -> TokenStream {
+    let parser =
+        syn::punctuated::Punctuated::<syn::Ident, syn::Token![,]>::parse_separated_nonempty;
+    let ids = match parser.parse(input) {
+        Ok(t) => t,
+        Err(e) => {
+            return TokenStream::from(e.to_compile_error());
+        }
+    };
+    let mut ids_iter = ids.iter();
+    // TODO: Handle errors
+    let world = ids_iter.next().unwrap();
+    let collection = ids_iter.next().unwrap();
+    let fields: Vec<Ident> = ids_iter.map(get_field_from_type).collect();
+    let captures: Vec<Ident> = fields
+        .iter()
+        .enumerate()
+        .map(|x| Ident::new(&("f".to_owned() + &x.0.to_string()), x.1.span()))
+        .collect();
+    let expanded = quote! {
+        itertools::izip!(
+            #(&mut #collection.#fields,)*
+            &#world.entities
+        )
+        .filter_map(|(#(#captures,)* e)| Some((#(#captures.as_mut()?,)* e)))
+    };
+    TokenStream::from(expanded)
+}
+
+#[proc_macro]
+pub fn create_world(_: TokenStream) -> TokenStream {
+    let expanded = quote! {
+        World::new(Box::new(LameEcsComponentCollection::default()))
+    };
+    TokenStream::from(expanded)
+}
+
+fn get_field_from_type(ty: &Ident) -> Ident {
+    let name = ty.to_string().to_lowercase() + "s";
+    Ident::new(&name, ty.span())
+}
+
+fn get_field_names(data: &syn::DataStruct) -> Option<Vec<&syn::Ident>> {
+    match &data.fields {
+        Fields::Named(fs) => Some(fs.named.iter().filter_map(|f| f.ident.as_ref()).collect()),
+        _ => None,
+    }
+}
+
+fn is_type(ty: &syn::Type, name: &str) -> bool {
+    match ty {
+        syn::Type::Path(p)
+            if p.qself.is_none()
+                && p.path.segments.len() == 1
+                && p.path.segments.first().unwrap().ident == name =>
+        {
+            true
+        }
+        _ => false,
+    }
+}
+
+fn get_inner_type(ty: &syn::Type) -> Option<&syn::Type> {
+    let argument = match ty {
+        syn::Type::Path(p) => match &p.path.segments.first()?.arguments {
+            syn::PathArguments::AngleBracketed(a) => a.args.first()?,
+            _ => {
+                return None;
+            }
+        },
+        _ => {
+            return None;
+        }
+    };
+    match argument {
+        syn::GenericArgument::Type(ty) => Some(ty),
+        _ => None,
+    }
+}
+
+fn get_component_type(ty: &syn::Type) -> Option<&syn::Type> {
+    if !is_type(ty, "Vec") {
+        return None;
+    }
+    let ty = get_inner_type(ty)?;
+    if !is_type(ty, "Option") {
+        return None;
+    }
+    get_inner_type(ty)
+}
+
+fn get_component_types(data: &syn::DataStruct) -> Option<Vec<&syn::Type>> {
+    let field_types: Vec<&syn::Type> = match &data.fields {
+        Fields::Named(fs) => Some(fs.named.iter().map(|f| &f.ty).collect()),
+        _ => None,
+    }?;
+    let mut component_types = Vec::new();
+    component_types.reserve(field_types.len());
+    for ty in &field_types {
+        component_types.push(get_component_type(ty)?);
+    }
+    Some(component_types)
+}
---
+++ lame-ecs/src/lib.rs
@@ -0,0 +1,346 @@
+pub use lame_ecs_macro::*;
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+pub struct Entity {
+    id: i64,
+}
+
+impl Entity {
+    pub fn new(id: i64) -> Entity {
+        Entity { id }
+    }
+
+    pub fn id(&self) -> i64 {
+        self.id
+    }
+}
+
+pub trait Components {
+    fn push_none(&mut self);
+    fn remove(&mut self, index: usize);
+    fn as_any(&self) -> &dyn std::any::Any;
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any;
+}
+
+pub trait Component: Sized {
+    fn get_vec(components: &mut dyn Components) -> &mut Vec<Option<Self>>;
+}
+
+pub struct World {
+    entity_count: i64,
+    pub entities: Vec<Entity>,
+    pub components: Box<dyn Components>,
+}
+
+pub fn downcast_components<T: 'static>(c: &dyn Components) -> &T {
+    c.as_any()
+        .downcast_ref::<T>()
+        .expect("wrong components type")
+}
+
+pub fn downcast_components_mut<T: 'static>(c: &mut dyn Components) -> &mut T {
+    c.as_any_mut()
+        .downcast_mut::<T>()
+        .expect("wrong components type")
+}
+
+impl World {
+    pub fn new(components: Box<dyn Components>) -> World {
+        World {
+            entity_count: 0,
+            entities: Vec::new(),
+            components,
+        }
+    }
+
+    pub fn new_entity(&mut self) -> Entity {
+        if self.entity_count == core::i64::MAX {
+            panic!("maximum number of entities reached");
+        }
+        let id = self.entity_count;
+        self.entity_count += 1;
+        self.components.push_none();
+        self.entities.push(Entity { id });
+        Entity { id }
+    }
+
+    pub fn remove_entity(&mut self, entity: Entity) {
+        let index = self
+            .get_index(entity)
+            .expect("trying to remove invalid entity");
+        self.entities.remove(index);
+        self.components.remove(index);
+    }
+
+    pub fn get_component<C: Component>(&mut self, entity: Entity) -> Option<&mut C> {
+        self.get_component_option(entity).as_mut()
+    }
+
+    pub fn add_component<C: Component>(&mut self, entity: Entity, component: C) {
+        *self.get_component_option::<C>(entity) = Some(component);
+    }
+
+    pub fn remove_component<C: Component>(&mut self, entity: Entity) {
+        self.get_component_option::<C>(entity).take();
+    }
+
+    pub fn is_alive(&self, entity: Entity) -> bool {
+        self.get_index(entity).is_some()
+    }
+
+    fn get_index(&self, entity: Entity) -> Option<usize> {
+        self.entities.iter().position(|x| x.id == entity.id)
+    }
+
+    fn get_component_option<C: Component>(&mut self, entity: Entity) -> &mut Option<C> {
+        let index = self.get_index(entity).expect("operation on invalid entity");
+        &mut C::get_vec(self.components.as_mut())[index]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[derive(Debug, PartialEq)]
+    struct Foo {
+        m: i32,
+    }
+
+    #[derive(Debug, PartialEq)]
+    struct Bar {
+        m: String,
+    }
+
+    create_component_collection_struct!(Foo, Bar);
+
+    #[test]
+    fn new_entity() {
+        let mut world = create_world!();
+        let e0 = world.new_entity();
+        let e1 = world.new_entity();
+        assert_ne!(e0, e1);
+    }
+
+    #[test]
+    fn add_component() {
+        let mut world = create_world!();
+        let e0 = world.new_entity();
+
+        let c = Foo { m: 21 };
+        world.add_component::<Foo>(e0, c);
+
+        let x = world.get_component::<Foo>(e0);
+        assert!(!x.is_none());
+        assert_eq!(x.unwrap().m, 21);
+    }
+
+    #[test]
+    fn add_existing_component() {
+        let mut world = create_world!();
+        let e0 = world.new_entity();
+
+        let c = Foo { m: 42 };
+        world.add_component::<Foo>(e0, c);
+
+        let c = Foo { m: 17 };
+        world.add_component::<Foo>(e0, c);
+
+        let x = world.get_component::<Foo>(e0);
+        assert!(!x.is_none());
+        assert_eq!(x.unwrap().m, 17);
+    }
+
+    #[test]
+    fn remove_component() {
+        let mut world = create_world!();
+        let e0 = world.new_entity();
+
+        let c = Foo { m: 0 };
+        world.add_component::<Foo>(e0, c);
+
+        {
+            let c = world.get_component::<Foo>(e0);
+            assert!(!c.is_none());
+        }
+
+        world.remove_component::<Foo>(e0);
+        let c = world.get_component::<Foo>(e0);
+        assert!(c.is_none());
+    }
+
+    #[test]
+    fn remove_entity() {
+        let mut world = create_world!();
+        let e0 = world.new_entity();
+
+        let foo_comp = Foo { m: 0 };
+        world.add_component::<Foo>(e0, foo_comp);
+        let bar_comp = Bar {
+            m: "bar".to_owned(),
+        };
+        world.add_component::<Bar>(e0, bar_comp);
+
+        let e1 = world.new_entity();
+        let foo_comp = Foo { m: 42 };
+        world.add_component(e1, foo_comp);
+
+        {
+            let int_comp = world.get_component::<Foo>(e0);
+            assert!(!int_comp.is_none());
+            let str_comp = world.get_component::<Bar>(e0);
+            assert!(!str_comp.is_none());
+            let int_comp = world.get_component::<Foo>(e1);
+            assert!(!int_comp.is_none());
+        }
+
+        assert!(world.is_alive(e0));
+        world.remove_entity(e0);
+        assert!(!world.is_alive(e0));
+
+        let int_comp = world.get_component::<Foo>(e1);
+        assert!(!int_comp.is_none());
+
+        assert!(world.is_alive(e1));
+        world.remove_entity(e1);
+        assert!(!world.is_alive(e1));
+    }
+
+    #[test]
+    fn multiple_entities() {
+        let mut world = create_world!();
+        let e0 = world.new_entity();
+        let e1 = world.new_entity();
+
+        let i0 = Foo { m: 10 };
+        world.add_component::<Foo>(e0, i0);
+        let i1 = Foo { m: 30 };
+        world.add_component::<Foo>(e1, i1);
+
+        {
+            let i0 = world.get_component::<Foo>(e0);
+            assert!(!i0.is_none());
+            assert_eq!(i0.unwrap().m, 10);
+
+            let i1 = world.get_component::<Foo>(e1);
+            assert!(!i1.is_none());
+            assert_eq!(i1.unwrap().m, 30);
+        }
+
+        let bar_comp = Bar {
+            m: "bar".to_owned(),
+        };
+        world.add_component::<Bar>(e1, bar_comp);
+        assert!(world.get_component::<Bar>(e0).is_none());
+        assert!(!world.get_component::<Bar>(e1).is_none());
+
+        assert!(world.is_alive(e1));
+        world.remove_entity(e1);
+        assert!(!world.is_alive(e1));
+        assert!(!world.get_component::<Foo>(e0).is_none());
+    }
+
+    #[test]
+    #[should_panic]
+    fn remove_missing_entity() {
+        let mut world = create_world!();
+        let entity = world.new_entity();
+        world.remove_entity(entity);
+        world.remove_entity(entity);
+    }
+
+    #[test]
+    #[should_panic]
+    fn get_component_on_missing_entity() {
+        let mut world = create_world!();
+        let entity = world.new_entity();
+        world.add_component(
+            entity,
+            Bar {
+                m: String::from("foo"),
+            },
+        );
+        assert!(world.get_component::<Bar>(entity).is_some());
+        world.remove_entity(entity);
+        world.get_component::<Bar>(entity);
+    }
+
+    #[test]
+    #[should_panic]
+    fn remove_component_on_missing_entity() {
+        let mut world = create_world!();
+        let entity = world.new_entity();
+        world.add_component(
+            entity,
+            Bar {
+                m: String::from("foo"),
+            },
+        );
+        assert!(world.get_component::<Bar>(entity).is_some());
+        world.remove_entity(entity);
+        world.remove_component::<Bar>(entity);
+    }
+
+    #[test]
+    #[should_panic]
+    fn add_component_to_missing_entity() {
+        let mut world = create_world!();
+        let entity = world.new_entity();
+        world.add_component(
+            entity,
+            Bar {
+                m: String::from("foo"),
+            },
+        );
+        assert!(world.get_component::<Bar>(entity).is_some());
+        world.remove_entity(entity);
+        world.add_component::<Foo>(entity, Foo { m: 42 });
+    }
+
+    #[test]
+    fn test_component_iter_macro() {
+        let mut world = create_world!();
+        let e0 = world.new_entity();
+        let e1 = world.new_entity();
+        world.add_component(e0, Foo { m: 42 });
+        world.add_component(
+            e0,
+            Bar {
+                m: "bar0".to_owned(),
+            },
+        );
+        world.add_component(e1, Foo { m: 84 });
+        let components = get_component_collection!(world);
+
+        let mut iter = component_iter!(world, components, Foo, Bar);
+        assert_eq!(
+            iter.next().unwrap(),
+            (
+                &mut Foo { m: 42 },
+                &mut Bar {
+                    m: "bar0".to_owned()
+                },
+                &e0
+            )
+        );
+        assert_eq!(iter.next(), None);
+
+        let mut iter = component_iter!(world, components, Foo);
+        assert_eq!(iter.next().unwrap(), (&mut Foo { m: 42 }, &e0));
+        assert_eq!(iter.next().unwrap(), (&mut Foo { m: 84 }, &e1));
+        assert_eq!(iter.next(), None);
+
+        let mut iter = component_iter!(world, components, Bar);
+        assert_eq!(
+            iter.next().unwrap(),
+            (
+                &mut Bar {
+                    m: "bar0".to_owned()
+                },
+                &e0
+            )
+        );
+        assert_eq!(iter.next(), None);
+    }
+}
